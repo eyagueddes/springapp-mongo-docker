49bad3c96af1265993e3ecbc4c1d3f9bc7dc5db1
eya gueddes <eyagueddes1@gmail.com>
2021-12-23 16:30:01 +0100

    first commit

---
+++ src/main/java/com/mycompany/obitemservice/ObItemServiceApplication.java
@@ -0,0 +1,15 @@
+package com.mycompany.obitemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObItemServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ObItemServiceApplication.class, args);
+		System.out.println("server is started");
+	}
+
+}
